06308fc14abd49087c0664eb42c76c4c5f89685f
Gustavo Almeida <73246160+gustavoalmei@users.noreply.github.com>
2021-09-01 21:21:12 -0300

    Adicionando arquivos

---
+++ JS/basico/script.js
@@ -0,0 +1,11 @@
+// alert("Hello world")
+
+var a;
+var b;
+
+a = "hd"
+b = a
+
+// Váriaveis com letras maiúsculas são diferentes de váriaveis com letras minúsculas. Ex: caixaGrande é diferente de caixagrande.
+
+console.log(b)
