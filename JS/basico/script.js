67b31bd7a9c100be91db66252b74b40ba967b474
Gustavo Almeida <73246160+gustavoalmei@users.noreply.github.com>
2021-09-04 19:47:16 -0300

    atulizandor arquivo JS

--- JS/basico/script.js
+++ JS/basico/script.js
@@ -1,11 +1,116 @@
+// alerta 
 // alert("Hello world")
 
-var a;
-var b;
+// declarando váriaveis
+// var a;
+// var b;
 
-a = "hd"
-b = a
+// a = "hd"
+// b = a
 
 // Váriaveis com letras maiúsculas são diferentes de váriaveis com letras minúsculas. Ex: caixaGrande é diferente de caixagrande.
 
-console.log(b)
+// console.log(b)
+
+// tipos de dados
+
+/*
+Boolean / true ou false
+null / não tem valor nenhum
+undefined / não foi definido nenhum valor, podendo ter ou não
+number / número
+string / caracter
+*/
+
+// Opções aritméticas
+
+/*
++ adição
+- subtração
+* multiplicação
+/ divisão
+% módulo
+++ incremento
+-- decremento
+*/
+
+// var a = 10;
+// var b = 25;
+// var c = a % b;
+
+// console.log(c)
+
+// Atribuições
+
+/*
+=        x = y       x = y
++=       x = y       x = x + y
+-=       x = y       x = x - y
+*=       x = y       x = x * y
+/=       x = y       x = x / y
+%=       x = y       x = x % y
+*/  
+
+// var a = 30;
+
+// a += 10;
+
+// a++
+
+// É a mesma coisa que a = a + 10
+
+// console.log(a)
+
+// Operadores comparativos
+
+// == igual a 
+// === valor igual a igual 
+// != não é igual 
+// !== não é igual ou não igual 
+// > maior que 
+// < menor que 
+// > = maior que ou igual a 
+// < = menor que ou igual a 
+
+// console.log( 2 == "2") o valor 2 é igual
+
+// console.log( 2 === "2") o valor 2 é igual, porém, o tipo do valor é diferente, um é um Number, e outro é uma string 
+
+// console.log( 3 != "3") os valores não são diferentes
+// console.log( 3 !== "3") os valores não são diferentes, porém, o tipo é diferente
+
+// Operadores lógicos
+
+// && e  / para ser true, os dois valores devem ser truem, senao, será false
+// || ou  / apenas um valor precisa ser true para o resultado ser true, caso contrário, sairá false
+// Negativo ! / irá negar o valor
+
+// (&&)
+// var idade = 25;
+
+// var maior20 = idade > 20;
+// var menor30 = idade < 30;
+// var entre = menor30 && maior20;
+
+// console.log("Maior que vinte?", maior20)
+// console.log("Menor que vinte?", menor30)
+// console.log(entre)
+
+// (||)
+// var idade = 15;
+// var menor18 = idade <= 18;
+// var maior60 = idade >= 60;
+// var vale = menor18 || maior60;
+
+// console.log("Maior que vinte?", maior60)
+// console.log("Menor que vinte?", menor18)
+// console.log(vale)
+
+// (Negativo)
+// var idade = 35;
+
+// var maior20 = idade > = 20;
+// var menor20 = !maior20;
+
+// console.log("Maior que vinte?", maior20)
+// console.log("Menor que vinte?", menor20)
