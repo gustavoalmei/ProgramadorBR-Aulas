adc4f0a218b3a9a19819f681c6eac732450921b6
Gustavo Almeida <73246160+gustavoalmei@users.noreply.github.com>
2021-11-03 23:03:48 -0300

    add new

--- JS/intermediario/script.js
+++ JS/intermediario/script.js
@@ -291,3 +291,56 @@ function mouseout(){
 //     conteudo += `<li>${i}</li>`
 // }
 // lista.innerHTML = conteudo
+
+// HTML
+
+// preload = "auto" - irá carregar uma parte do video
+// preload = "metadata" - irá carregar algumas informações do vídeo apenas
+// preload = "none" - o video irá carregar apenas quando o usuario clicar no play
+
+// controls = mostrará os controles do player
+// autoplay = iniciara o vídeo automaticamente 
+// poster = é usado para escolher uma thumbnail do video
+
+// <video controls preload="" src="Tillmann - AUGE ft. Caio Luccas (Prod. @_yungdrum).mp4" poster="thumbnail"></video>
+
+// Adicionando JS ao video
+
+// let video = document.getElementById("video1")
+
+// function play(){
+//     video.play()
+// }
+
+// function pause(){
+//     video.pause()
+// }
+
+// function stopVideo(){
+//     video.pause()
+//     video.currentTime = 0;
+// }
+
+// function upVolume(){
+//     video.volume += 0.05;
+// }
+
+// function downVolume(){
+//     video.volume -= 0.05;
+// }
+
+// HTML
+// <div class="container">
+
+// <video class="video" id="video1"
+// src="Tillmann - AUGE ft. Caio Luccas (Prod. @_yungdrum).mp4"
+// ></video>
+
+// <div class="controler">
+// <i onclick="play()" class="fas fa-play"></i>
+// <i onclick="pause()" class="fas fa-pause"></i>
+// <i onclick="stopVideo()" class="fas fa-stop"></i>
+// <i onclick="upVolume()" class="fas fa-volume-up"></i>
+// <i onclick="downVolume()" class="fas fa-volume-down"></i>
+// </div>
+// </div>
